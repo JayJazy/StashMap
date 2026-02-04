e702fbbf50d533ddab618709a0ceb39a8c66724b
JayJayz <jeong-yeongseog@jeong-yeongseog-ui-MacBookPro.local>
2026-02-05 05:39:06 +0900

    [feat] 디자인 시스템 폰트 및 색상 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,11 +9,7 @@ android {
 applyFeatureDependencies()
 
 dependencies {
-    // Feature modules
     implementation(project(":feature:main"))
-    implementation(project(":feature:home"))
-    implementation(project(":feature:stash"))
-    implementation(project(":feature:profile"))
 
     // App specific dependencies
     implementation(libs.androidx.lifecycle.runtime.ktx)
